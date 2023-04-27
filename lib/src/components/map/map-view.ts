952124309cbfecdffbce411acd0e6459f432e131
Cristian Sedaboni <cristian.sedaboni@gullivernet.com>
2023-04-27 16:54:11 +0200

    Added view override on map component

--- lib/src/components/map/map-view.ts
+++ lib/src/components/map/map-view.ts
@@ -14,122 +14,128 @@ export class MapView extends ParentView {
         const props = node.props as MapProps;
         const nodes = props?.children ? props.children : [];
 
-        const placeholderWidth = context.options.style.placeholder.width;
-        const placeholderHeight = context.options.style.placeholder.height;
+        let mapView!: MapView;
 
-        const element = DomHelper.svg('g', {
-            class: "map",
-        });
-        element.classList.add(...getNodeClasses(node));
-
-        const stepView = await StepView.create(node, context);
-
-        const mapLabelWidth = stepView.width;
-        const mapLabelHeight = stepView.height;
-        const sequenceGroup = DomHelper.svg('g', {
-            class: "map-children-wrapper",
-        });
-
-        const mapLabelIcon = DomHelper.svg('g', {
-            class: "map-label-icon",
-        });
-        mapLabelIcon.appendChild(DomHelper.svg('circle', {
-            r: 12,
-            class: 'circle-label-icon',
-            'stroke-width': 1.25,
-        }));
-
-        const iconSize = 20;
-        mapLabelIcon.appendChild(DomHelper.svg('image', {
-            href: loopIcon,
-            width: iconSize,
-            height: iconSize,
-            x: -iconSize / 2,
-            y: -iconSize / 2,
-        }));
-
-        stepView.element.appendChild(mapLabelIcon);
-
-        const childrenContainer = DomHelper.svg('g', {
-            class: "children-container",
-        });
-        const childrenContainerBg = DomHelper.svg('rect', {
-            class: "children-container-bg",
-            rx: 6,
-        });
-
-        childrenContainer.appendChild(childrenContainerBg);
-        sequenceGroup.appendChild(childrenContainer);
-        element.appendChild(sequenceGroup);
-        element.appendChild(stepView.element);
-        parent.appendChild(element);
-
-        const flowMode = context.designerState.flowMode;
-
-        // Create sequence
-        const sequenceComponent = await Sequence.create(nodes, node, childrenContainer, context);
-        const childrenContainerBgLeftOffset = 30;
-        const childrenContainerBgTopOffset = 10;
-
-        let totalWidth = 0;
-        let totalHeight = 0;
-        let joinX;
-        let joinY;
-
-        let childrenContainerBgWidth;
-
-        if (flowMode === 'vertical') {
-            totalWidth = sequenceComponent.view.width + childrenContainerBgLeftOffset;
-            totalHeight = sequenceComponent.view.height + mapLabelHeight - childrenContainerBgTopOffset;
-
-            joinX = totalWidth / 2;
-            joinY = totalHeight;
-
-            childrenContainerBgWidth = totalWidth;
-        }
-        else {
-            totalWidth = sequenceComponent.view.width + mapLabelWidth - childrenContainerBgTopOffset;
-            totalHeight = sequenceComponent.view.height + childrenContainerBgLeftOffset;
-
-            joinX = totalWidth;
-            joinY = totalHeight / 2;
-
-            childrenContainerBgWidth = totalWidth - childrenContainerBgTopOffset;
+        if (context.userDefinedOverriders?.overrideView && context.userDefinedOverriders.overrideView?.map) {
+            mapView = await context.userDefinedOverriders.overrideView.map({ node, parent: parentNode, parentElement: parent }, context) as MapView;
         }
 
-        childrenContainerBg.setAttribute('width', `${childrenContainerBgWidth}px`);
-        childrenContainerBg.setAttribute('height', `${totalHeight}px`);
+        if (!mapView) {
+            const element = DomHelper.svg('g', {
+                class: "map",
+            });
+            element.classList.add(...getNodeClasses(node));
 
-        // Output connection dot
-        const endConnection = DomHelper.svg('circle', {
-            r: 5,
-            cx: joinX,
-            cy: flowMode === 'vertical' ? (joinY + childrenContainerBgTopOffset) : joinY,
-            class: 'output',
-            fill: "black",
-            stroke: "black",
-        });
-        childrenContainer.appendChild(endConnection);
+            const stepView = await StepView.create(node, context);
 
-        if (flowMode === 'vertical') {
-            DomHelper.translate(childrenContainerBg, 0, childrenContainerBgTopOffset);
-            DomHelper.translate(stepView.element, (totalWidth - mapLabelWidth) / 2, 0);
-            DomHelper.translate(sequenceComponent.view.element, childrenContainerBgLeftOffset / 2, placeholderHeight);
-            DomHelper.translate(mapLabelIcon, stepView.width / 2, stepView.height);
+            const mapLabelWidth = stepView.width;
+            const mapLabelHeight = stepView.height;
+            const sequenceGroup = DomHelper.svg('g', {
+                class: "map-children-wrapper",
+            });
 
-            totalHeight = totalHeight + childrenContainerBgTopOffset;
+            const mapLabelIcon = DomHelper.svg('g', {
+                class: "map-label-icon",
+            });
+            mapLabelIcon.appendChild(DomHelper.svg('circle', {
+                r: 12,
+                class: 'circle-label-icon',
+                'stroke-width': 1.25,
+            }));
+
+            const iconSize = 20;
+            mapLabelIcon.appendChild(DomHelper.svg('image', {
+                href: loopIcon,
+                width: iconSize,
+                height: iconSize,
+                x: -iconSize / 2,
+                y: -iconSize / 2,
+            }));
+
+            stepView.element.appendChild(mapLabelIcon);
+
+            const childrenContainer = DomHelper.svg('g', {
+                class: "children-container",
+            });
+            const childrenContainerBg = DomHelper.svg('rect', {
+                class: "children-container-bg",
+                rx: 6,
+            });
+
+            childrenContainer.appendChild(childrenContainerBg);
+            sequenceGroup.appendChild(childrenContainer);
+            element.appendChild(sequenceGroup);
+            element.appendChild(stepView.element);
+            parent.appendChild(element);
+
+            const flowMode = context.designerState.flowMode;
+
+            // Create sequence
+            const sequenceComponent = await Sequence.create(nodes, node, childrenContainer, context);
+            const childrenContainerBgLeftOffset = 30;
+            const childrenContainerBgTopOffset = 10;
+
+            let totalWidth = 0;
+            let totalHeight = 0;
+            let joinX;
+            let joinY;
+
+            let childrenContainerBgWidth;
+
+            if (flowMode === 'vertical') {
+                totalWidth = sequenceComponent.view.width + childrenContainerBgLeftOffset;
+                totalHeight = sequenceComponent.view.height + mapLabelHeight - childrenContainerBgTopOffset;
+
+                joinX = totalWidth / 2;
+                joinY = totalHeight;
+
+                childrenContainerBgWidth = totalWidth;
+            }
+            else {
+                totalWidth = sequenceComponent.view.width + mapLabelWidth - childrenContainerBgTopOffset;
+                totalHeight = sequenceComponent.view.height + childrenContainerBgLeftOffset;
+
+                joinX = totalWidth;
+                joinY = totalHeight / 2;
+
+                childrenContainerBgWidth = totalWidth - childrenContainerBgTopOffset;
+            }
+
+            childrenContainerBg.setAttribute('width', `${childrenContainerBgWidth}px`);
+            childrenContainerBg.setAttribute('height', `${totalHeight}px`);
+
+            // Output connection dot
+            const endConnection = DomHelper.svg('circle', {
+                r: 5,
+                cx: joinX,
+                cy: flowMode === 'vertical' ? (joinY + childrenContainerBgTopOffset) : joinY,
+                class: 'output',
+                fill: "black",
+                stroke: "black",
+            });
+            childrenContainer.appendChild(endConnection);
+
+            if (flowMode === 'vertical') {
+                DomHelper.translate(childrenContainerBg, 0, childrenContainerBgTopOffset);
+                DomHelper.translate(stepView.element, (totalWidth - mapLabelWidth) / 2, 0);
+                DomHelper.translate(sequenceComponent.view.element, childrenContainerBgLeftOffset / 2, context.options.style.placeholder.height);
+                DomHelper.translate(mapLabelIcon, stepView.width / 2, stepView.height);
+
+                totalHeight = totalHeight + childrenContainerBgTopOffset;
+            }
+            else {
+                DomHelper.translate(childrenContainerBg, childrenContainerBgTopOffset, 0);
+                DomHelper.translate(stepView.element, 0, (totalHeight - stepView.height) / 2);
+                DomHelper.translate(sequenceComponent.view.element, mapLabelWidth, joinY - sequenceComponent.view.joinY);
+                DomHelper.translate(mapLabelIcon, stepView.width, stepView.height / 2);
+            }
+
+            mapView = new MapView(element, parent, totalWidth, totalHeight, joinX, joinY, sequenceComponent);
         }
-        else {
-            DomHelper.translate(childrenContainerBg, childrenContainerBgTopOffset, 0);
-            DomHelper.translate(stepView.element, 0, (totalHeight - stepView.height) / 2);
-            DomHelper.translate(sequenceComponent.view.element, mapLabelWidth, joinY - sequenceComponent.view.joinY);
-            DomHelper.translate(mapLabelIcon, stepView.width, stepView.height / 2);
-        }
 
-        await addHasErrorIfNecessary(element, node, parentNode, context);
+        await addHasErrorIfNecessary(mapView.element, node, parentNode, context);
 
-        const mapView = new MapView(element, parent, totalWidth, totalHeight, joinX, joinY, sequenceComponent);
-        mapView._selectableElement = stepView.element;
+        mapView._selectableElement = mapView.element;
         return mapView;
     }
 
